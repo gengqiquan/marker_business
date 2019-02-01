19e7bcdd16093385986bed20796756cc51b63d32
耿其权 <542400966@qq.com>
2019-02-01 18:40:06 +0800

    商家注册

--- lib/ui/home/HomeUI.dart
+++ lib/ui/home/HomeUI.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:marker_business/ui/login/RegisterMarketUI.dart';
+import 'package:marker_business/utils/Colour.dart';
 
 ///auther:gengqiquan
 ///date:2019/1/30
 ///description:HomeUI
 
 class HomeUI extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => new _HomeUIState();
 }
@@ -20,14 +20,115 @@ class _HomeUIState extends State<HomeUI> {
 //        middle: const Text("首页"),
 //      ),
       body: new Container(
-        color: Colors.yellow,
-        child: new Center(
-          child: new RaisedButton(onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (BuildContext context) => RegisterMarketUI()));
-          }),
-        ),
-      ),
+          color: Colour.back,
+          child: new Column(
+            children: <Widget>[
+              new Stack(
+                children: <Widget>[
+                  new Image.asset(
+                    "images/diwen_fp.png",
+                    height: 180,
+                    fit: BoxFit.fill,
+                  ),
+                  new Column(
+                    children: <Widget>[
+                      new Padding(
+                          padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).padding.top + 48)),
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          buildButton("images/add_product.png", "添加商品", () {}),
+                          buildButton("images/putaway.png", "商品上架", () {}),
+                          buildButton("images/order.png", "订单管理", () {}),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              new Padding(padding: EdgeInsets.only(top: 10)),
+              new Container(
+                height: 50,
+                padding: EdgeInsets.only(left: 34),
+                alignment: Alignment.centerLeft,
+                color: Colors.white,
+                width: double.infinity,
+                child: new Text(
+                  "客户管理",
+                  style: TextStyle(color: Colors.black, fontSize: 16),
+                ),
+              ),
+              new Divider(
+                height: 0.5,
+              ),
+              new Row(
+                children: <Widget>[
+                  buildButton("images/add_product.png", "添加商品", () {}),
+                  buildButton("images/putaway.png", "商品上架", () {}),
+                  buildButton("images/order.png", "订单管理", () {}),
+                ],
+              ),
+              new Container(
+                color: Colors.white,
+                child: new GridView.builder(
+                  shrinkWrap: true,
+                    itemCount: list.length,
+                    gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3),
+                    itemBuilder: (context, index) => buildItem(context, index)),
+              ),
+            ],
+          )),
     );
   }
+
+  buildButton(String path, String text, Function onTap) {
+    return new Flexible(
+        child: new InkWell(
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          new Image.asset(
+            path,
+            width: 39,
+            height: 39,
+          ),
+          new Padding(padding: EdgeInsets.only(top: 11)),
+          new Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          )
+        ],
+      ),
+    ));
+  }
+
+  buildItem(BuildContext context, int index) {
+    var item = list[index];
+    return new InkWell(
+        child: new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        new Image.asset(
+          item["path"],
+          width: 39,
+          height: 39,
+        ),
+        new Padding(padding: EdgeInsets.only(top: 11)),
+        new Text(
+          item["text"],
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        )
+      ],
+    ));
+  }
+
+  List list = [
+    {"path": "images/product.png", "text": "体验客户"},
+    {"path": "images/product.png", "text": "购买客户"},
+    {"path": "images/product.png", "text": "分销代理"},
+    {"path": "images/product.png", "text": "特权设置"},
+    {"path": "images/product.png", "text": "客户评论"},
+  ];
 }
--- lib/utils/Colour.dart
+++ lib/utils/Colour.dart
@@ -7,7 +7,7 @@ class Colour {
   static const Color gray999 = Color(0xff999999);
   static const Color gray666 = Color(0xff666666);
   static const Color gray333 = Color(0xff333333);
-  static const Color back = Color(0xfff0f0f0);
+  static const Color back = Color(0xfff4f7fe);
   static const Color themeAccent = Color(0xFF448AFF);
   static const Color themeAccent15 = Color(0x15448AFF);
   static const Color themeAccentE0 = Color(0xe0448AFF);
